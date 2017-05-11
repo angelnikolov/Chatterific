3aed41cdc9870db5f95f038f697bbd815eaf9e9d
Angel Nikolov <angel.nikolov@mentormate.com>
2017-05-11 19:14:27 +0300

    Tweaked functions, added storage.rules.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const language = require('@google-cloud/language')();
 const functions = require('firebase-functions');
+const gcs = require('@google-cloud/storage')();
+const vision = require('@google-cloud/vision')();
+const exec = require('child-process-promise').exec;
 // Import and initialize the Firebase Admin SDK.
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
@@ -18,60 +21,112 @@ admin.initializeApp(functions.config().firebase);
 //     });
 // });
 
-// // Annotates messages using the Cloud Natural Language API
-// exports.annotateMessages = functions.database.ref('/messages/{messageId}').onWrite((event) => {
-//     const snapshot = event.data;
-//     const messageId = event.params.messageId;
+// Annotates messages using the Cloud Natural Language API
+exports.annotateMessages = functions.database.ref('/messages/{messageId}').onWrite((event) => {
+    const snapshot = event.data;
+    const messageId = event.params.messageId;
 
-//     // Only annotate new messages.
-//     if (snapshot.previous.val() || !snapshot.val().text) {
-//         return;
-//     }
+    // Only annotate new messages.
+    if (snapshot.previous.val() || !snapshot.val().text) {
+        return;
+    }
 
-//     // Annotation arguments.
-//     const text = snapshot.val().text;
-//     const options = {
-//         entities: true,
-//         sentiment: true
-//     };
+    // Annotation arguments.
+    const text = snapshot.val().text;
+    const options = {
+        entities: true,
+        sentiment: true
+    };
 
-//     console.log('Annotating new message.');
-//     return admin.database().ref(`/messages`).once('value').then((snapshot) => {
-//         console.log(snapshot.numChildren());
-//         let finalText = ''; snapshot.forEach((message) => {
-//             console.log('current Text', message.val().text);
-//             console.log('final text', finalText);
-//             finalText = finalText + ' ' + message.val().text
-//         });
-//         console.log(finalText);
-//         if (snapshot.numChildren() > 5) {
-//             // Detect the sentiment and entities of the new message.
-//             return language.annotate(finalText, options)
-//                 .then((result) => {
-//                     console.log('Saving annotations.');
+    console.log('Annotating new message.');
 
-//                     // Update the message with the results.
-//                     return admin.database().ref(`/sentiments`).push({
-//                         sentiment: result[0].sentiment,
-//                         entities: result[0].entities.map((entity) => {
-//                             return {
-//                                 name: entity.name,
-//                                 salience: entity.salience
-//                             };
-//                         })
-//                     });
-//                 });
-//         }
-//         else {
-//             return true;
-//         }
-//     })
-// });
+    // Detect the sentiment and entities of the new message.
+    return language.annotate(text, options)
+        .then((result) => {
+            console.log('Saving annotations.');
+
+            // Update the message with the results.
+            return admin.database().ref(`/messages/${messageId}`).update({
+                sentiment: result[0].sentiment,
+                entities: result[0].entities.map((entity) => {
+                    return {
+                        name: entity.name,
+                        salience: entity.salience
+                    };
+                })
+            });
+        });
+});
 
 // Adds newly authenticated users to the users collection in the database
 exports.addNewUsersToCollection = functions.auth.user().onCreate((event) => {
     const user = event.data;
     console.log('Saving user with a name', user.displayName)
-    return admin.database().ref('users').push(
-        user);
+    return new Promise((resolve, reject) => admin.database().ref('users').push(
+        user).then(() => { resolve(); }))
 });
+
+
+
+// Blurs uploaded images that are flagged as Adult or Violence.
+exports.blurOffensiveImages = functions.storage.object().onChange((event) => {
+    const object = event.data;
+    // Exit if this is a deletion or a deploy event.
+    if (object.resourceState === 'not_exists') {
+        return console.log('This is a deletion event.');
+    } else if (!object.name) {
+        return console.log('This is a deploy event.');
+    }
+
+    const messageId = object.name.split('/')[1];
+    const bucket = gcs.bucket(object.bucket);
+    const file = bucket.file(object.name);
+
+    return admin.database().ref(`/messages/${messageId}/moderated`).once('value')
+        .then((snapshot) => {
+            console.log(snapshot.val());
+            // The image has already been moderated.
+            if (snapshot.val()) {
+                return;
+            }
+
+            // Check the image content using the Cloud Vision API.
+            return vision.detectSafeSearch(file);
+        })
+        .then((safeSearchResult) => {
+            if (safeSearchResult && (safeSearchResult[0].adult || safeSearchResult[0].violence)) {
+                console.log('The image', object.name, 'has been detected as inappropriate.');
+                return blurImage(object.name, bucket);
+            } else {
+                console.log('The image', object.name, 'has been detected as OK.');
+            }
+        });
+});
+
+// Blurs the given image located in the given bucket using ImageMagick.
+function blurImage(filePath, bucket, metadata) {
+    const fileName = filePath.split('/').pop();
+    const tempLocalFile = `/tmp/${fileName}`;
+    const messageId = filePath.split('/')[1];
+
+    // Download file from bucket.
+    return bucket.file(filePath).download({ destination: tempLocalFile })
+        .then(() => {
+            console.log('Image has been downloaded to', tempLocalFile);
+            // Blur the image using ImageMagick.
+            return exec(`convert ${tempLocalFile} -channel RGBA -blur 0x24 ${tempLocalFile}`);
+        })
+        .then(() => {
+            console.log('Image has been blurred');
+            // Uploading the Blurred image back into the bucket.
+            return bucket.upload(tempLocalFile, { destination: filePath });
+        })
+        .then(() => {
+            console.log('Blurred image has been uploaded to', filePath);
+            // Indicate that the message has been moderated.
+            return admin.database().ref(`/messages/${messageId}`).update({ moderated: true });
+        })
+        .then(() => {
+            console.log('Marked the image as moderated in the database.');
+        });
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Observable } from 'rxjs/Observable';
 import { User } from './shared/models/user.model';
 import { FirebaseListObservable } from 'angularfire2/database';
-import { AngularFire, FirebaseAuthState, FirebaseObjectObservable } from 'angularfire2';
-import { Component } from '@angular/core';
+import { FirebaseApp, AngularFire, FirebaseAuthState, FirebaseObjectObservable } from 'angularfire2';
+import { Inject, Component } from '@angular/core';
 import { UserInfo } from "firebase";
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
@@ -11,6 +11,7 @@ import 'rxjs/add/operator/mergeAll';
 import 'rxjs/add/operator/filter';
 import 'rxjs';
 import * as firebase from 'firebase';
+const LOADING_IMAGE_URL = 'assets/spinner.gif';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,11 +21,13 @@ export class AppComponent {
   public messages: Observable<any[]>;
   public sentiment: FirebaseListObservable<any[]>;
   public users$: Observable<any[]>;
-  public lastMessage$ : Observable<any>;
+  public lastMessage$: Observable<any>;
+  public totalSentiment$: Observable<any>;
   public selectedUser: UserInfo = null;
 
+  fbApp: any;
   public message: string;
-  public user: UserInfo;
+  public user: UserInfo = null;
   selectUser(selectedUser: UserInfo) {
 
     this.selectedUser = selectedUser;
@@ -35,7 +38,7 @@ export class AppComponent {
           equalTo: this.user.uid
         }
       })
-      .map(messages => messages.filter(message=>message.to === this.selectedUser.uid)),
+        .map(messages => messages.filter(message => message.to === this.selectedUser.uid)),
 
       this.af.database.list('/messages', {
         query: {
@@ -43,14 +46,34 @@ export class AppComponent {
           equalTo: this.user.uid
         }
       })
-      .map(messages => messages.filter(message=>message.from === this.selectedUser.uid)),
+        .map(messages => messages.filter(message => message.from === this.selectedUser.uid)),
     )
       .map((messages: [Array<any>, Array<any>]) => [...messages[0], ...messages[1]].sort((a, b) => a.timestamp - b.timestamp));
 
-      this.lastMessage$ = this.messages.map(messages=>messages[messages.length-1]);
+    this.lastMessage$ = this.messages.map(messages => messages[messages.length - 1]);
+    this.totalSentiment$ = this.messages
+      .map(messages => messages
+        // .filter((message, index, array) => array.indexOf(array.find(_message=>_message.text===message.text)) === index)
+        .reduce((accumulator, currentMessage, index) => {
+          if (index === 1) {
+            accumulator = accumulator.sentiment ? accumulator.sentiment.score : 0;
+          }
+          currentMessage.sentiment = currentMessage.sentiment || { score: 0 }
+          return Number(accumulator) + Number(currentMessage.sentiment.score);
+        })
+      )
+      .map(totalSentiment => {
+        if (totalSentiment > 1) {
+          totalSentiment = 1
+        }
+        if (totalSentiment < -1) {
+          totalSentiment = -1;
+        }
+        return totalSentiment;
+      })
   }
-  constructor(public af: AngularFire) {
-    ;
+  constructor(public af: AngularFire, @Inject(FirebaseApp) fbApp: any) {
+    this.fbApp = fbApp;
     this.sentiment = <FirebaseListObservable<any[]>>this.af.database.list('/sentiments')
       .do(objects => { console.log(objects) })
       .map(objects => { return objects[objects.length - 1] ? objects[objects.length - 1].sentiment : null })
@@ -66,12 +89,63 @@ export class AppComponent {
   }
 
   logout() {
-    this.af.auth.logout();
+    this.af.auth.logout().then(() => {
+      this.user = null;
+      this.messages = Observable.of([]);
+      this.users$ = Observable.of([]);
+    });
   }
 
+  // TODO: Refactor into image message form component
+  onImageClick(event: any) {
+    event.preventDefault();
+    document.getElementById('mediaCapture').click();
+  }
+  // TODO: Refactor into image message form component
+  saveImageMessage(event: any) {
+    event.preventDefault();
+    const file = event.target.files[0];
+
+    // Clear the selection in the file picker input.
+    const imageForm = <HTMLFormElement>document.getElementById('image-form');
+    imageForm.reset();
+
+
+    // Check if the user is signed-in
+
+    // We add a message with a loading icon that will get updated with the shared image.
+    const messages = this.af.database.list('/messages');
+    messages.push({
+      imageUrl: LOADING_IMAGE_URL,
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      text: null,
+      user: this.user,
+      from: this.user.uid,
+      to: this.selectedUser.uid
+
+    }).then((data) => {
+      // Upload the image to Cloud Storage.
+      const filePath = `${this.user.uid}/${data.key}/${file.name}`;
+      return this.fbApp.storage().ref(filePath).put(file)
+        .then((snapshot) => {
+          // Get the file's Storage URI and update the chat message placeholder.
+          const fullPath = snapshot.metadata.fullPath;
+          const imageUrl = this.fbApp.storage().ref(fullPath).toString();
+          return this.fbApp.storage().refFromURL(imageUrl).getMetadata();
+        }).then((metadata) => {
+          // TODO: Instead of saving the download URL, save the GCS URI and
+          //       dynamically load the download URL when displaying the image
+          //       message.
+          return data.update({
+            imageUrl: metadata.downloadURLs[0]
+          });
+        });
+    });
+  }
   sendMessage($event) {
     $event.preventDefault();
-    this.af.database.list('/messages').push({ timestamp: firebase.database.ServerValue.TIMESTAMP, text: this.message, user: this.user, from: this.user.uid, to: this.selectedUser.uid });
+    this.af.database.list('/messages')
+      .push({ timestamp: firebase.database.ServerValue.TIMESTAMP, text: this.message, user: this.user, from: this.user.uid, to: this.selectedUser.uid });
     this.message = null;
   }
 }
